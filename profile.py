abca6207c0e9e922fb76d1cbecf1cda28c0d67f9
Michael Collins <98608611+MC965348@users.noreply.github.com>
2024-06-30 01:14:05 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,13 +8,11 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 # Create a XenVM
-for i in range(3):
+for i in range(2):
   if i == 0:
     node = request.XenVM("webserver")
-  elif i == 1: 
-    node = request.XenVM("observer")
   else:
-    node = request.XenVM("ldap")       
+    node = request.XenVM("observer")     
 
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
